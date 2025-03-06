b59efd66d0d9dfa01960ebaf673bcbc3af9276c6
jamesnlamug <jamesnlamug@gmail.com>
2025-03-05 23:52:55 -0800

    queen movement and capture code

--- Queen.js
+++ Queen.js
@@ -3,4 +3,69 @@ class Queen extends Piece {
 		super(row, column, side);
 		this.element.innerHTML = "This is a Queen";
 	}
+
+	getPossibleMoves(board) {
+		let possibleMoves = [];
+		
+		possibleMoves = possibleMoves.concat(this.getPossibleMovesDirection(board, -1, 0));
+		possibleMoves = possibleMoves.concat(this.getPossibleMovesDirection(board, 1, 0));
+		possibleMoves = possibleMoves.concat(this.getPossibleMovesDirection(board, 0, -1));
+		possibleMoves = possibleMoves.concat(this.getPossibleMovesDirection(board, 0, 1));
+		possibleMoves = possibleMoves.concat(this.getPossibleMovesDirection(board, -1, -1));
+		possibleMoves = possibleMoves.concat(this.getPossibleMovesDirection(board, -1, 1));
+		possibleMoves = possibleMoves.concat(this.getPossibleMovesDirection(board, 1, -1));
+		possibleMoves = possibleMoves.concat(this.getPossibleMovesDirection(board, 1, 1));
+		
+		return possibleMoves;
+	}
+
+	getPossibleMovesDirection(board, rowOffset, columnOffset) {
+		
+		let possibleMoves = [];
+
+		let currentPiece = this;
+		let nextPiece = Piece.direction(board, currentPiece, rowOffset, columnOffset);
+
+		while(nextPiece !== null && nextPiece.constructor.name == "EmptyPiece") {
+
+			possibleMoves.push(nextPiece);
+			currentPiece = nextPiece;
+			nextPiece = Piece.direction(board, currentPiece, rowOffset, columnOffset);
+
+		}
+		return possibleMoves;
+
+	}
+
+	getPossibleCaptures(board, lastMove="") {
+		let possibleCaptures = [];
+
+		possibleCaptures = possibleCaptures.concat(this.getPossibleCaptureDirection(board, -1, 0));
+		possibleCaptures = possibleCaptures.concat(this.getPossibleCaptureDirection(board, 1, 0));
+		possibleCaptures = possibleCaptures.concat(this.getPossibleCaptureDirection(board, 0, -1));
+		possibleCaptures = possibleCaptures.concat(this.getPossibleCaptureDirection(board, 0, 1));
+		possibleCaptures = possibleCaptures.concat(this.getPossibleCaptureDirection(board, -1, -1));
+		possibleCaptures = possibleCaptures.concat(this.getPossibleCaptureDirection(board, -1, 1));
+		possibleCaptures = possibleCaptures.concat(this.getPossibleCaptureDirection(board, 1, -1));
+		possibleCaptures = possibleCaptures.concat(this.getPossibleCaptureDirection(board, 1, 1));
+
+		return possibleCaptures;
+	}
+
+	getPossibleCaptureDirection(board, rowOffset, columnOffset) {
+		
+		let currentPiece = this;
+		let nextPiece = Piece.direction(board, currentPiece, rowOffset, columnOffset);
+
+		while(nextPiece !== null && nextPiece.constructor.name == "EmptyPiece") {
+
+			currentPiece = nextPiece;
+			nextPiece = Piece.direction(board, currentPiece, rowOffset, columnOffset);
+
+		}
+
+		if (nextPiece !== null && nextPiece.side == Piece.opposite(this.side)) return [nextPiece];
+		return [];
+
+	}
 }
