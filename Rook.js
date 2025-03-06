42437f7ec37675c7c78c67578e6a3b4c3087dd8e
jamesnlamug <jamesnlamug@gmail.com>
2025-03-05 23:41:50 -0800

    rook capture code

--- Rook.js
+++ Rook.js
@@ -27,13 +27,41 @@ class Rook extends Piece {
 		let nextPiece = Piece.direction(board, currentPiece, rowOffset, columnOffset);
 
 		while(nextPiece != null && nextPiece.constructor.name == "EmptyPiece") {
-			
+
 			possibleMoves.push(nextPiece);
 			currentPiece = nextPiece;
 			nextPiece = Piece.direction(board, currentPiece, rowOffset, columnOffset);
 
 		}
 		return possibleMoves;
+
+	}
+
+	getPossibleCaptures(board, lastMove="") {
+		let possibleCaptures = [];
+
+		possibleCaptures = possibleCaptures.concat(this.getPossibleCaptureDirection(board, -1, 0));
+		possibleCaptures = possibleCaptures.concat(this.getPossibleCaptureDirection(board, 1, 0));
+		possibleCaptures = possibleCaptures.concat(this.getPossibleCaptureDirection(board, 0, -1));
+		possibleCaptures = possibleCaptures.concat(this.getPossibleCaptureDirection(board, 0, 1));
+
+		return possibleCaptures;
+	}
+
+	getPossibleCaptureDirection(board, rowOffset, columnOffset) {
+		
+		let currentPiece = this;
+		let nextPiece = Piece.direction(board, currentPiece, rowOffset, columnOffset);
+
+		while(nextPiece !== null && nextPiece.constructor.name == "EmptyPiece") {
+
+			currentPiece = nextPiece;
+			nextPiece = Piece.direction(board, currentPiece, rowOffset, columnOffset);
+
+		}
+
+		if (nextPiece !== null && nextPiece.side == Piece.opposite(this.side)) return [nextPiece];
+		return [];
 
 	}
 }
